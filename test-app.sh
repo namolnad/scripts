315f4bd6fbe26ad531daf14853ce68353682c8ae
Dan Loman <daniel.h.loman@gmail.com>
2019-04-07 20:26:00 -0700

    Add basic test-app script

---
+++ test-app.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+base_dir=$(git rev-parse --show-toplevel)
+ide_path="$base_dir/build"
+project=Instacart.xcodeproj
+scheme=Instacart
+configuration=Debug
+simulator='iPhone 8'
+os=12.1
+
+command="test"
+
+for arg in "$@"; do
+    command="$command -only-testing:$arg"
+done
+
+echo """*Note* To limit executed tests, pass space-separated list \
+using one of the following patterns:
+    (target | target/class | target/class/test_name)"""
+
+echo """
+xcrun xcodebuild
+  -project $project
+  -scheme "$scheme"
+  -configuration $configuration
+  -destination "\'platform=iOS Simulator,name=$simulator,OS=$os\'"
+  $command
+"""
+
+xcrun xcodebuild \
+  -project $project \
+  -scheme "$scheme" \
+  -configuration $configuration \
+  -destination "platform=iOS Simulator,name=$simulator,OS=$os" \
+  $command
